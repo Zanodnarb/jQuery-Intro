7ec6ac12e245829090c04358a718d23512675baa
Zanodnarb <103510789+Zanodnarb@users.noreply.github.com>
2022-07-29 09:56:17 -0400

    added function
    added click function to make boxes

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ $(document).ready(onReady);
 let count = 0;
 
 function onReady() {
-    $('#scottsButton').on('click', scottsFunction);
+    $('#js-scottsButton').on('click', scottsFunction);
+    $('#js-container').on('click','.box', changeToPurple)
 }
 
 function scottsFunction() {
-    console.log('Clicky click');
     count++;
-    $('#howManyClicks').text(count);
+    $('#js-howManyClicks').text(count);
+    // append a new box div to my container:
+    $('#js-container').append(`
+    <div class="box"></div>
+    `);
+}
+
+function changeToPurple() {
+    console.log('Purple!')
 }
