2e090280e86a73591753cbf25571c28c4fb084a3
Zanodnarb <103510789+Zanodnarb@users.noreply.github.com>
2022-07-29 10:49:43 -0400

    update files

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ let count = 0;
 
 function onReady() {
     $('#js-scottsButton').on('click', scottsFunction);
-    $('#js-container').on('click','.box', changeToPurple)
+    $('#js-container').on('dblclick','.box', changeToPurple);
+    $('#js-container').on('click', '.js-btn-heart', heartPost)
 }
 
 function scottsFunction() {
@@ -12,10 +13,17 @@ function scottsFunction() {
     $('#js-howManyClicks').text(count);
     // append a new box div to my container:
     $('#js-container').append(`
-    <div class="box"></div>
+    <div class="box">
+        <button class="js-btn-heart">Heart</button>
+    </div>
     `);
 }
 
 function changeToPurple() {
     console.log('Purple!')
+    // here we will change it to purple
+}
+
+function heartPost() {
+    console.log('So in so liked this image!');
 }
